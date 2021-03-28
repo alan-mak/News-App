edcb8461fe2821d21759e884e8131fb06f65da5e
alan-mak <alanmak95@gmail.com>
2021-03-28 12:00:12 -0400

    Added sample information to page

--- server.js
+++ server.js
@@ -2,14 +2,21 @@
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
-const express    = require("express");
+const PORT = process.env.PORT || 8080;
+const express = require("express");
 const bodyParser = require("body-parser");
-const app        = express();
-const morgan     = require('morgan');
+const app = express();
+const morgan = require('morgan');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`)
 });
+
+/*-------------------------------------------------*/
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
